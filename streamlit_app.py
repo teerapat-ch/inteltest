bb9f221267c6087115feaec563025cab212db4d1
Teerapat Chanthawatchai <s6604062620018@email.kmutnb.ac.th>
2025-02-14 06:04:15 +0000

    aaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.header("Page 1", divider="gray")
+subject = st.text_input("Enter a subject name")
+submit_button = st.button("Enter")
+
+if submit_button:
+    st.write(f"You'll pass {subject}. Goodluck! :smile:")
